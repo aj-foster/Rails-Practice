47fc0a78312548491d03d62c7bc9289d5a36ea3a
AJ Foster <web@aj-foster.com>
2014-01-11 15:00:48 -0500

    Add resource routes.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,14 @@ Blog4::Application.routes.draw do
 	# You can have the root of your site routed with "root"
 	root 'posts#index'
 
+	resources :users do
+		resources :posts, :comments
+	end
+
+	resources :posts do
+		resources :comments
+	end
+
 	# Example of regular route:
 	#   get 'products/:id' => 'catalog#view'
 
